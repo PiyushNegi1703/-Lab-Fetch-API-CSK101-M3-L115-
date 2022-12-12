446aaa423f3434e042c37c6864a3588da435a5c6
PiyushNegi1703 <112609875+PiyushNegi1703@users.noreply.github.com>
2022-12-12 16:24:47 +0530

    2 files modified

--- weather.js
+++ weather.js
@@ -1,9 +1,19 @@
 //Complete the Weather API Backend part using openweathermap api
 
 // Progression 1: Create a function and fetch data using "fetch" from openweathermap api and display the data as given in reference image.
+function calcTime(offset) {
+  var d = new Date();
+
+  var utc = d.getTime() + d.getTimezoneOffset() * 60000;
+
+  var nd = new Date(utc + 3600000 * offset);
+
+  return nd.toDateString();
+}
+
 function weatherFetch() {
   fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=a93062c563cc61e1b58ef6f77d9e3bef'
+    'https://api.openweathermap.org/data/2.5/weather?q=Washington&appid=a93062c563cc61e1b58ef6f77d9e3bef'
   )
     .then((resp) => {
       return resp.json();
@@ -19,7 +29,7 @@ function weatherFetch() {
 
         <div id="city-name">
           <h3>${resp.name}, ${resp.sys.country}</h3>
-          <p>Sunday 27 September 2020</p>
+          <p>${calcTime(resp.timezone / 3600)}</p>
         </div>
 
         <div id="temp">
@@ -32,7 +42,6 @@ function weatherFetch() {
         resp.main.temp_min - 273
       )}°c</p>
         </div>`;
-      console.log(resp);
 
       document.body.append(container);
     });
